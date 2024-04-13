b2bcb2e482ed8c2f792603f4a79d4973318cd049
Sabrina Guimarães <ssousagu@emeal.nttdata.com>
2024-04-12 21:21:33 -0300

    adding a setup.py to init the project

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Data Crafters GenAI Agent',
+    version='0.1',
+    packages=find_packages(),
+    url='github.com/guimaraesabrina/data_crafters_guide_end_to_end_genai',
+    license='MIT',
+    author='Sabrina de S. Guimarães',
+    author_email='gsabsousa@gmail.com',
+    description='...',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
+
