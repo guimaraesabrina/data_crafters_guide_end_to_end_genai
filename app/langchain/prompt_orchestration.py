1a25121a4d08f1b61a74198acc657b91e52c9235
Sabrina Guimarães <ssousagu@emeal.nttdata.com>
2024-04-13 20:01:04 -0300

    fix PromptTemplate instance and consts access

--- app/langchain/prompt_orchestration.py
+++ app/langchain/prompt_orchestration.py
@@ -3,22 +3,30 @@ from langchain.prompts import PromptTemplate
 from app.common.text import PromptAgentConst
 from app.common.consts import TextAgentConst
 
-text = PromptAgentConst()
-consts = TextAgentConst()
-
 class SystemPromptGenerator:
 
     def __init__(self):
-        self.prompt_template = PromptTemplate.from_template(text.tool_orchestartion())
+        self.text = PromptAgentConst()
+        self.consts = TextAgentConst()
+        self.prompt_template = PromptTemplate() 
 
-    def prompt_chain_mlops(self, data_context=consts['mlops']):
+    def prompt_tool_orchestration(self):
+        return self.text.tool_orchestration()
+
+    def prompt_chain_mlops(self, data_context=None):
+        data_context = self.consts['mlops'] if data_context is None else data_context
         return self.prompt_template.format(data_context=data_context)
 
-    def prompt_chain_data_engineering(self, data_context=consts['data_engineering']):
+    def prompt_chain_data_engineering(self, data_context=None):
+        data_context = self.consts['data_engineering'] if data_context is None else data_context
         return self.prompt_template.format(data_context=data_context)
 
-    def prompt_chain_ml_engineering(self, data_context=consts['machine_learning_engineering']):
+    def prompt_chain_ml_engineering(self, data_context=None):
+        data_context = self.consts['machine_learning_engineering'] if data_context is None else data_context
         return self.prompt_template.format(data_context=data_context)
 
-    def prompt_chain_data_science(self, data_context=consts['data_science']):
+    def prompt_chain_data_science(self, data_context=None):
+        data_context = self.consts['data_science'] if data_context is None else data_context
         return self.prompt_template.format(data_context=data_context)
+
+
